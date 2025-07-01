e3491e36cff4471b58b0cf537f90d17abcacaae5
DreKord <drekordchannel@gmail.com>
2025-07-01 21:08:34 +0300

    Add Fabric API

---
+++ src/client/java/net/drekord/horizons/HorizonsClient.java
@@ -0,0 +1,10 @@
+package net.drekord.horizons;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HorizonsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/drekord/horizons/HorizonsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.drekord.horizons;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HorizonsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
